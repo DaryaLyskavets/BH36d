fd2c2c886fd5666f7fc5d8e454388bd084287649
DaryaLyskavets <daryareut22@gmail.com>
2022-12-23 13:02:44 +0300

    fourth task

--- lesson3_task4.py
+++ lesson3_task4.py
@@ -1,3 +1,3 @@
 number_1, number_2, number_3 = float(input()), float(input()), float(input())
-print('Количество отрицательных чисел = ', (number_1<0) + (number_2<0) + (number_3<0))
-print('Количество положительных чисел = ', (number_1>0) + (number_2>0) + (number_3>0))
+print('Количество отрицательных чисел = ', (number_1 < 0) + (number_2 < 0) + (number_3 < 0))
+print('Количество положительных чисел = ', (number_1 > 0) + (number_2 > 0) + (number_3 > 0))
