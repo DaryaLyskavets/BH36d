063471631beb826c7d7a54c03fdea0a1e19b6b55
DaryaLyskavets <daryareut22@gmail.com>
2022-12-28 08:44:02 +0300

    dictionary

--- lesson4_task3.py
+++ lesson4_task3.py
@@ -1,3 +1,7 @@
 n = int(input('Введите число:', ))
-dictionary = {i: {'name': input('Введите имя:', ), 'email': input('Введите email:', )} for i in range(n + 1)}
+dictionary = {i:
+                  {'name': input('Введите имя:', ),
+                   'email': input('Введите email:', )
+                   }
+              for i in range(n + 1)}
 print(dictionary)
