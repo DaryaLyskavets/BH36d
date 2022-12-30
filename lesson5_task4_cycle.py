57124020a7627904125fa8f9fa60118605860699
DaryaLyskavets <daryareut22@gmail.com>
2022-12-30 12:05:25 +0300

    coins

---
+++ lesson5_task4_cycle.py
@@ -0,0 +1,16 @@
+# Количество монет
+price = int(input())
+counter = 0
+while price >= 25:
+    price = price - 25
+    counter += 1
+while 10 <= price < 25:
+    price = price - 10
+    counter += 1
+while 5 <= price < 10:
+    price = price - 5
+    counter += 1
+while 1 <= price < 5:
+    price = price - 1
+    counter += 1
+print(counter)
