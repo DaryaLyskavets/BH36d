a58372b16dc3427bd966faa92a7f5c30c95684b0
DaryaLyskavets <daryareut22@gmail.com>
2022-12-30 10:06:20 +0300

    calculator

--- lesson5_task2.py
+++ lesson5_task2.py
@@ -1,22 +1,22 @@
 # Сделать калькулятор: у пользователя спрашивается число,
 # потом действие и второе число
-number = int(input())
+number = float(input())
 action = input()
-number_2 = int(input())
+number_2 = float(input())
 
 if action == '+':
-    print(number + number_2)
+    answer = number + number_2
 elif action == '-':
-    print(number - number_2)
+    answer = number - number_2
 elif action == '*':
-    print(number * number_2)
-elif action == '/':
-    print(number / number_2)
+    answer = number * number_2
+elif action == '/' and number_2 != 0:
+    answer = number / number_2
+elif number_2 == 0:
+    answer = 'На ноль делить нельзя!'
 else:
-    print('Введите +, -, * или /')
+    answer = 'Введите +, -, * или /'
+print(answer)
 
-try:
-    number_2 = 0
-except ZeroDivisionError:
-    print('На ноль делить нельзя')
+
 
