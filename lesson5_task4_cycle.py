6df832778da0a63c4b52f43db59fe4e5e05803b6
DaryaLyskavets <daryareut22@gmail.com>
2022-12-30 14:34:46 +0300

    variant 2

--- lesson5_task4_cycle.py
+++ lesson5_task4_cycle.py
@@ -3,6 +3,13 @@ price = int(input())
 print(price // 25 + price % 25 // 10 + price % 25 % 10 // 5 + price % 5)
 
 counter = 0
+for i in (25, 10, 5, 1):
+    while price >= i:
+        counter += 1
+        price = price - i
+print(counter)
+
+
 while price >= 25:
     price = price - 25
     counter += 1
