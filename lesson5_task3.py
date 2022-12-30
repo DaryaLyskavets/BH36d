dd082c9335467d1cdb39fc6adabe5b96e6ea7573
DaryaLyskavets <daryareut22@gmail.com>
2022-12-30 14:11:57 +0300

    5 lines of even numbers

--- lesson5_task3.py
+++ lesson5_task3.py
@@ -1,8 +1,19 @@
 # **Вывести четные числа от 2 до N по 5 в строку
 
 n = int(input())
-even_num = []
+count = 0
+line = ''
 for i in range(2, n + 1, 2):
-    even_num.append(i)
-for j in range(0, len(even_num), 5):
-    print(even_num[j:j + 5])
+    count += 1
+    if count % 5 != 0:
+        line += str(i) + ' '
+    elif count % 5 == 0:
+        line += str(i) + ' ' + '\n'
+print(line)
+
+# n = int(input())
+# even_num = []
+# for i in range(2, n + 1, 2):
+#     even_num.append(i)
+# for j in range(0, len(even_num), 5):
+#     print(even_num[j:j + 5])
