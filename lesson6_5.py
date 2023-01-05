eb376215ff5af5ef19549bad11f3207ad446cac3
DaryaLyskavets <daryareut22@gmail.com>
2023-01-05 20:01:51 +0300

    reverse numbers

--- lesson6_5.py
+++ lesson6_5.py
@@ -1,6 +1,7 @@
 # Дан список чисел, необходимо его развернуть
 my_list = [45, 1, 3, 5, 0, -1, 9, -3, 5]
 
+
 def reverse_list(numbers):
     rev_numbers = []
     for i in range(len(numbers) - 1, -1, -1):
