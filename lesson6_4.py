7cbf063a4d54a97b4aaa9c5f2f7267db90ac76bf
DaryaLyskavets <daryareut22@gmail.com>
2023-01-05 19:13:45 +0300

    only strings

---
+++ lesson6_4.py
@@ -0,0 +1,12 @@
+# Дан список содержащий в себе различные типы данных, отфильтровать таким
+# образом, чтобы остались только строки
+my_list = ['hello', 3, '45', 'python', 356, ['l', 'm'], '!']
+
+def only_string(list):
+    new_list = []
+    for i in list:
+        if isinstance(i, str):
+            new_list.append(i)
+    return new_list
+
+print(*only_string(my_list))
