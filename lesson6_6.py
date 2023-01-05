cc3252a4275bfb52b6d7c22dc80acb9d2119f800
DaryaLyskavets <daryareut22@gmail.com>
2023-01-05 20:51:35 +0300

    sort even and odd numbers

---
+++ lesson6_6.py
@@ -0,0 +1,19 @@
+# Дан список рандомных чисел, необходимо отсортировать его в виде, сначала четные, потом нечётные
+from random import randint
+
+numbers = [randint(-100, 100) for _ in range(randint(0, 10))]
+print(numbers)
+
+
+def sort_even_odd(number):
+    even = []
+    odd = []
+    for i in number:
+        if i % 2 == 0:
+            even.append(i)
+        else:
+            odd.append(i)
+    return even + odd
+
+
+print(sort_even_odd(numbers))
