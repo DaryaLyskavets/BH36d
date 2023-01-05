49e487e6ac39855821d85f468c8c144053632768
DaryaLyskavets <daryareut22@gmail.com>
2023-01-05 12:25:08 +0300

    bin number

--- lesson6_1.py
+++ lesson6_1.py
@@ -6,6 +6,11 @@ def num_bin(number):
         str_bin = str(number % 2) + str_bin
         number //= 2
     print(str_bin)
+    number = 0
+    len_bin = len(str_bin)
+    for i in range(0, len_bin):
+        number += int(str_bin[i]) * (2**(len_bin - i - 1))
+    print(number)
 
 num = int(input())
 num_bin(num)
