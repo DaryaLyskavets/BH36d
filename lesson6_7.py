4e9bacb9dc7d17dce4970d019fd5c87520646d6f
DaryaLyskavets <daryareut22@gmail.com>
2023-01-09 15:34:15 +0300

    summa neighbour

--- lesson6_7.py
+++ lesson6_7.py
@@ -1,17 +1,13 @@
 # Дан список чисел, необходимо для каждого элемента посчитать сумму его
 # соседей, для крайних чисел одним из соседей является число с противоположной стороны списка
-from random import randint
-
-numbers = [randint(1, 100) for _ in range(randint(3, 15))]
-print(numbers)
-
 
 def count_summ(num):
     a = []
-    for i in range(0, len(num)-1):
+    for i in range(0, len(num) - 1):
         a.append(num[i - 1] + num[i + 1])
     a.append(num[-2] + num[0])
     print(a)
 
 
+numbers = [1, 2, 3, 4, 5]
 count_summ(numbers)
