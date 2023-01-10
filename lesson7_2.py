a67561524c33866abd12608a3e860b029aad2f3f
DaryaLyskavets <daryareut22@gmail.com>
2023-01-10 16:15:04 +0300

    average height

--- lesson7_2.py
+++ lesson7_2.py
@@ -9,13 +9,15 @@ humans = [{'gender': 'm', 'height': '186'},
           {'gender': 'w', 'height': '162'},
           {'gender': 'm', 'height': '181'}]
 
-
-def average_height(humans):
+def average_height(humans, gender):
     total = 0
-    all_height = [i.get('height') for i in humans]
+    all_height = []
+    for i in humans:
+        if i.get('gender') == gender:
+            all_height.append(i.get('height'))
     for i in all_height:
         total += int(i)
     return total / len(all_height)
 
-
-print(average_height(humans))
+gender = input()
+print(average_height(humans, gender))
