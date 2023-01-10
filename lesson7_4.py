5f42d44151349582eb156b8655aac6255ed7d9f2
DaryaLyskavets <daryareut22@gmail.com>
2023-01-10 15:18:27 +0300

    sales receipt

---
+++ lesson7_4.py
@@ -0,0 +1,23 @@
+# Необходимо выпустить кассовый чек (вернуть строку), в котором будут
+# указаны названия товаров, их количество, общая стоимость и общая стоимость с учетом скидок
+
+products = [{'id': '01', 'title': 'milk', 'price': 1.98, 'discount': 1},
+            {'id': '02', 'title': 'water', 'price': 1.32, 'discount': 1},
+            {'id': '03', 'title': 'potatoes', 'price': 2.5, 'discount': 3},
+            {'id': '04', 'title': 'chocolate', 'price': 3.6, 'discount': 5},
+            {'id': '05', 'title': 'steak', 'price': 24, 'discount': 12},
+            {'id': '06', 'title': 'fish', 'price': 21, 'discount': 15},
+            ]
+food_basket = [('01', 2), ('03', 5), ('04', 1), ('05', 1)]
+
+def sales_bill(products, food_basket):
+    user_bill = []
+    for i in products:
+        if food_basket[0][0] == i.get('id'):
+            user_bill.append(i.get('title') + ' ' + str(food_basket[0][1]) + ' '+
+                             str(i.get('price')*food_basket[0][1]) + ' ' +
+                             str(i.get('price')*food_basket[0][1] -
+                             i.get('price')*food_basket[0][1]/100*i.get('discount')))
+    print(user_bill)
+
+sales_bill(products,food_basket)
