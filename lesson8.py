76f46a670607ee7d29bb768c2cc6f3029bb837d0
DaryaLyskavets <daryareut22@gmail.com>
2023-01-13 13:26:39 +0300

    rectangle

---
+++ lesson8.py
@@ -0,0 +1,27 @@
+# Написать класс Rectangle
+
+class Rectangle:
+
+    def __init__(self, a: list[int], b: list[int], c: list[int], d: list[int]) -> None:
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+
+    def perimeter(self) -> int:
+        distance_1 = ((self.a[0] - self.c[0]) ** 2 + (self.a[1] - self.c[1]) ** 2) ** 0.5
+        distance_2 = ((self.a[0] - self.d[0]) ** 2 + (self.a[1] - self.d[1]) ** 2) ** 0.5
+        perimeter = (distance_1 + distance_2) * 2
+        return perimeter
+
+    def square(self) -> int:
+        distance_1 = ((self.a[0] - self.c[0]) ** 2 + (self.a[1] - self.c[1]) ** 2) ** 0.5
+        distance_2 = ((self.a[0] - self.d[0]) ** 2 + (self.a[1] - self.d[1]) ** 2) ** 0.5
+        square = distance_1 * distance_2
+        return square
+
+
+points = Rectangle(a=[3, -2], b=[-1, 4], c=[-1, -2], d=[3, 4])
+points_2 = Rectangle(a=[4, -2], b=[-1, 5], c=[-1, -2], d=[4, 5])
+print(points.perimeter())
+print(Rectangle([8, -5], [3, 5], [3, -5], [8, 5]).square())
