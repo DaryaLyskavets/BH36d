ac6e55a355c7900423554cb08db6d7e4eafdc02c
DaryaLyskavets <daryareut22@gmail.com>
2023-01-16 13:16:37 +0300

    les8

--- main.py
+++ main.py
@@ -123,4 +123,48 @@
 #
 # print(find_all(tar, sym))
 
+# class Student:
+#     def __init__(self, first_name: str, group: int, marks: list[int]):
+#         self.first_name = first_name
+#         self.group = group
+#         self.marks = marks
+#     def __str__(self) -> str:
+#         return f'Student: {self.first_name=} {self.group} {self.marks}'
+#     @staticmethod
+#     def student_sort(student: list['Student']) -> list['Student']:
+#
+#         return student
 
+class Category:
+    categories = []
+
+    @classmethod
+    def add(cls, name: str):
+        for i in cls.categories:
+            if name not in cls.categories:
+                cls.categories.append(name)
+                return cls.categories.index(name)
+            else:
+                raise ValueError
+
+    @classmethod
+    def get(cls, index: int):
+        return cls.categories[index]
+
+    @classmethod
+    def delete(cls, index: int) -> None:
+        # if index in range(len(cls.categories():
+        #     del cls.categories[index]
+        try:
+            del cls.categories[index]
+        except IndexError:
+            pass
+
+    @classmethod
+    def update(cls,index: int, new_name: str):
+        if index not in range(len(cls.categories())):
+            cls.add(new_name)
+        elif new_name not in cls.categories:
+            cls.categories[index] = new_name
+        else:
+            raise ValueError
